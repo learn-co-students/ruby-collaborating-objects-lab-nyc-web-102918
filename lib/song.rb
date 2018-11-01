de83ac14e13df61211ae88320f9d0d76bdaa5151
vibhu mahendru <vibhumahendru@vibhus-MacBook-Pro.local>
2018-11-01 18:19:03 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+require 'pry'
+require_relative './artist'
+require_relative './mp3_importer'
+
+class Song
+ attr_accessor :artist
+  attr_reader :artist, :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def self.new_by_filename(file_name)
+
+    newAr = file_name.split(" - ")
+    song = self.new(newAr[1])
+    artist_name = Artist.find_or_create_by_name(newAr[0])
+
+    # all.find do |element|
+    #   element.name == newAr[0]
+    # end
+    song.artist = artist_name
+    artist_name.songs << song
+    song
+  end
+
+
+end
